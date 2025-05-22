ed6cc5d216234a4b5860e5e9ca4227b5ac102785
william-ferreira <william-ferreira@specchio.info>
2025-05-22 16:33:06 -0300

    Estrutura inicial do banco de dados para o sistema de OS

---
+++ sql/ddl.sql
@@ -0,0 +1,29 @@
+begin transaction;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE tecnicos (
+    id uuid primary key default(gen_random_uuid()),
+    nome text not null,
+    email text not null, 
+    lat double precision not null,
+    lng double precision not null,
+    disponivel boolean not null
+);
+
+CREATE TABLE ordens_servico_status (
+    id uuid primary key default(gen_random_uuid()),
+    nome text not null
+);
+CREATE TABLE ordens_servico (
+    id uuid primary key default(gen_random_uuid()),
+    protocolo text,
+    descricao text,
+    endereco text,
+    lat double precision not null,
+    lng double precision not null,
+    data_agendada timestamptz not null,
+    previsao time not null,
+    tecnico_id uuid not null references tecnicos(id),
+    status_id uuid not null references ordens_servico_status(id)
+);
+commit;
