595efd9f5a75a7d51bcaa320ba28d1b1690cb70d
william-ferreira <william-ferreira@specchio.info>
2025-05-24 23:52:38 -0300

    Mudei a rota para o endpoint /ordens-servico para que aceite json no post. Aproveitei e criei uma função para gerar mensagem de erro

--- routes/routes.php
+++ routes/routes.php
@@ -1,22 +1,47 @@
 <?php
+
 require_once __DIR__ . '/../services/AuthMiddleware.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-switch($uri) {
+function defaultErrors ($status, $msg) {
+    http_response_code($status);
+    echo json_encode(['erro' => $msg]);
+    exit;
+}
+// Validar o json na entrada dos dados
+function checkJson ($json) {
+    $data = json_decode($json, true);
+
+    // Verifica se o JSON foi decodificado corretamente
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        defaultErrors(404, "JSON malformado");
+        exit;
+    }
+    return $data;
+}
+
+switch ($uri) {
     case '/token':
         if ($method != 'POST') {
-            http_response_code(405);
-            echo json_encode(['erro' => 'Método não permitido. Use POST']);
+            defaultErrors(405, "Método não permitido. Use POST");
             exit;
         }
         require_once __DIR__ . '/../controllers/AuthController.php';
         (new AuthController())->genToken($_POST['login'], $_POST['password']);
         break;
     case '/ordens-servico':
+        if ($method != 'POST') {
+            defaultErrors(405, "Método não permitido. Use POST");
+            exit;
+        }
+        // Valida a conexão
         $payload = checkAuth();
         require_once __DIR__ . '/../controllers/OsController.php';
-        (new OsController())->newOs($_POST['descricao'], $_POST['endereco'], $_POST['lat'], $_POST['lng'], $_POST['data_agendada']);
+        // Pega os dados do POST e valida o JSON
+        $post_json = file_get_contents('php://input');
+        $json = checkJson($post_json);
+        (new OsController())->newOs($json);
         break;
     default:
         http_response_code(404);
