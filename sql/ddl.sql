1a2b91d4228cc4f8816daf83ba2e8b6cfbb35587
william-ferreira <william-ferreira@specchio.info>
2025-05-25 01:50:54 -0300

    Adicionei registros na tabela de status das ordens de serviço

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -28,4 +28,8 @@ CREATE TABLE ordens_servico (
 );
 create table usuario (id uuid primary key default(gen_random_uuid()), login text not null, password text not null);
 insert into usuario (id, login, password) values ('77334473-f2aa-4906-bdac-e8e7778e0824', 'root', crypt('root', gen_salt('bf'))) on conflict(id) do nothing;
+
+INSERT INTO ordens_servico_status (id, nome) VALUES ('a8827f04-f26c-4ba7-8593-d3572b22baf9', 'pendente') on conflict (id) do nothing;
+INSERT INTO ordens_servico_status (id, nome) VALUES ('2b704832-ffd9-4d38-b99d-043943b9e278', 'em_andamento') on conflict (id) do nothing;
+INSERT INTO ordens_servico_status (id, nome) VALUES ('2c5c7397-e49e-4b92-a40f-2c0297dd4386', 'concluida') on conflict (id) do nothing;
 commit;
