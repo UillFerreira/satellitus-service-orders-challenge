cd3a5d36a3ae7a1f81a4a0c15b8ea7c4fd13449a
william-ferreira <william-ferreira@specchio.info>
2025-05-25 01:39:20 -0300

    Coloquei a função de conexão no banco de dados dentro de uma classe para poder abstrair a abertura da conexão. Com essa mudança, tive que modificar o código que busca o login

--- config/db.php
+++ config/db.php
@@ -1,11 +1,20 @@
 <?php
 
 require_once __DIR__ . '/env.php';
+class pgsql {
+    private function conn () {
+        $pg = "pgsql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME');
+        return new PDO($pg, getenv('DB_USER'), getenv('DB_PASS'), [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    }
+    public function query ($query, $param) {
 
-function psqlConn()
-{
-    $pg = "pgsql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME');
-    return new PDO($pg, getenv('DB_USER'), getenv('DB_PASS'), [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+        $pdo = $this->conn();
+        $pdo->beginTransaction();
+        $stmt = $pdo->prepare($query);
+        $stmt->execute($param);
+        $pdo->commit();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
--- models/Usuario.php
+++ models/Usuario.php
@@ -4,13 +4,9 @@ require_once __DIR__ . '/../config/db.php';
 
 class Usuario
 {
-    public static function getLogin($login)
-    {
-        $pdo = psqlConn();
-        $stmt = $pdo->prepare("SELECT * FROM usuario WHERE login = :login");
-        $stmt->bindParam(':login', $login);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+    public static function getLogin($login) {
+        $pgsql = new pgsql();
+        $ret = $pgsql->query("SELECT * FROM usuario WHERE login = :login", array(":login" =>  $login));
+        return $ret;
     }
 }
