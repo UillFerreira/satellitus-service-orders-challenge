7fe57551c61781b357904c8ea257e94250cb5e68
william-ferreira <william-ferreira@specchio.info>
2025-05-24 23:53:46 -0300

    Coloquei o tratamento dos campos do json

--- controllers/OsController.php
+++ controllers/OsController.php
@@ -1,17 +1,23 @@
 <?php
 
-class OsController
-{
-    public function newOs($descricao = null, $endereco = null, $lat = null, $lng = null, $data_agendamento = null)
-    {
-        if (!$descricao || (!$endereco && (!$lat && !$lng)) || !$data_agendamento) {
+require_once __DIR__ . '/../services/OsService.php';
+class OsController {
+    public function newOs($json) {
+        $descricao          = isset($json['descricao']) ? $json['descricao'] : null;
+        $endereco           = isset($json['endereco']) ? $json['endereco'] : null;
+        $lat                = isset($json['lat']) ? $json['lat'] : null;
+        $lng                = isset($json['lng']) ? $json['lng'] : null;
+        $data_agendada      = isset($json['data_agendada']) ? $json['data_agendada'] : null;
+        $previsao           = isset($json['previsao']) ? $json['previsao'] : null;
+                               
+        if (!$descricao || (!$endereco && (!$lat && !$lng)) || !$data_agendada || !$previsao) {
             http_response_code(400);
             echo json_encode(['erro' => 'Faltando parâmetros']);
             return;
         }
 
-        //$auth = new AuthService();
-        //$token = $auth->checkLogin($login, $password);
+        $os = new OsService();
+        $os->createOs($descricao, $endereco, $lat, $lng, $data_agendada, $previsao);
 
     }
 }
